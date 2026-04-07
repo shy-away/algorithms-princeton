e49b2038ae46c4dd3e40f5de9b7bea3542d4fdcf
shy_away <149800599+shy-away@users.noreply.github.com>
2026-04-07 15:47:37 -0500

    feat: interpret mode, help text, suppress notes closes #6

--- demo.sh
+++ demo.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+USAGE="Usage: $0 [-i] <target_class>"
+
 export CLASSPATH=".:lib/algs4.jar"
 
+INTERPRET_FLAG=""
+
+while getopts "i" opt; do
+  case $opt in
+    i) INTERPRET_FLAG="-Xint";;
+    *) echo "$USAGE" >&2; exit 1;;
+  esac
+done
+
+
+shift $((OPTIND-1))
+
+if [ $# -lt 1 ]; then
+  echo "$USAGE" >&2
+  exit 1
+fi
+
 TARGET=$1
-
 DIR=$(dirname "$TARGET")
-CLASS=$(basename "$TARGET")
 
-javac -cp "$CLASSPATH" "$DIR"/*.java
+javac -cp "$CLASSPATH" "$DIR"/*.java 2>&1 | awk '!/Note/'
 
 shift 1
 
-java -ea -cp "$CLASSPATH:$DIR" "$TARGET" "$@"
+java -ea $INTERPRET_FLAG -cp "$CLASSPATH:$DIR" "$TARGET" "$@"
 
-find $DIR -name "*.class" -type f -delete
+find "$DIR" -name "*.class" -type f -delete
