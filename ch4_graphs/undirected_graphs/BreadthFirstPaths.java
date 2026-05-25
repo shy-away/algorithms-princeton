d9b88d06ed652ef9f6dd1c2b57ac50ec6a08911e
shy_away <149800599+shy-away@users.noreply.github.com>
2026-05-24 22:58:10 -0500

    feat: BreadthFirstPaths distTo() in constant time

--- ch4_graphs/undirected_graphs/BreadthFirstPaths.java
+++ ch4_graphs/undirected_graphs/BreadthFirstPaths.java
@@ -1,16 +1,19 @@
 package ch4_graphs.undirected_graphs;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
 
 public class BreadthFirstPaths {
   private boolean[] marked;
   private int[] edgeTo;
+  private int[] distTo;
   private final int s;
 
   public BreadthFirstPaths(Graph G, int s) {
     marked = new boolean[G.V()];
     edgeTo = new int[G.V()];
+    distTo = new int[G.V()];
     this.s = s;
     bfs(G, s);
   }
@@ -19,8 +22,9 @@ public class BreadthFirstPaths {
     // use queue instead of stack (call stack in DFS)
     Queue<Integer> queue = new Queue<>();
 
-    // mark starting location
+    // process starting location
     marked[s] = true;
+    distTo[s] = 0;
     queue.enqueue(s);
 
     while (!queue.isEmpty()) {
@@ -31,6 +35,11 @@ public class BreadthFirstPaths {
       for (int w : G.adj(v)) {
         if (!marked[w]) {
           edgeTo[w] = v;
+
+          distTo[w] = distTo[v] + 1;
+          // Note: distTo[v] is never null because v is from the queue,
+          // and all vertices on the queue have been processed (starting with s).
+
           marked[w] = true;
           queue.enqueue(w);
         }
@@ -56,5 +65,27 @@ public class BreadthFirstPaths {
     path.push(s);
 
     return path;
+  }
+
+  public int distTo(int v) {
+    return distTo[v];
+  }
+
+  public static void main(String[] args) {
+    // only test distTo()
+
+    Graph G = new Graph(new In(args[0]));
+    int start = Integer.parseInt(args[1]);
+    BreadthFirstPaths bfp = new BreadthFirstPaths(G, start);
+
+    for (int v = 0; v < G.V(); v++) {
+      System.out.print(v + ": ");
+
+      if (bfp.hasPathTo(v)) {
+        System.out.println(bfp.distTo(v));
+      } else {
+        System.out.println("Not connected");
+      }
+    }
   }
 }
